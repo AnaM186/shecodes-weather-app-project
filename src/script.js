038468f89e04fe284ca67893971a4eaf8abacca3
AnaM186 <annamoskalenko1501@gmail.com>
2025-05-21 10:59:26 +0100

    Added the search functionality using API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,26 @@
-function searchCity(event) {
+function showTemperature(response) {
+  let currentTemperature = response.data.temperature.current;
+  let temperature = Math.round(currentTemperature);
+  let temperatureElement = document.querySelector("#temperature-element");
+  temperatureElement.innerHTML = temperature;
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "tf4aa9e540f4cdoa5b4833a44f190a87";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSearchForm(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchFormInput.value;
+
+  searchCity(searchFormInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchCity);
+searchForm.addEventListener("submit", handleSearchForm);
+
+searchCity("Gateshead");
